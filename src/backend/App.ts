ceaad44de3a4c1c2aba4e0f63e8ec97538d9b942
JulenJR <julenjr@hotmail.com>
2023-04-27 13:11:37 +0200

    multer update

--- src/backend/App.ts
+++ src/backend/App.ts
@@ -15,6 +15,29 @@ export class App {
 const app = express();
 const port = process.env.PORT || 8000;
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb)=>{
+    cb(null, 'upload/');
+  },
+  filename: (req, file, cb)=>{
+    cb(null, file.originalname);
+  }
+});
+
+
+const upload = multer({
+  storage: storage,
+  fileFilter: (req, file, cb)=>{
+    console.log('Mimetype:   ', file.mimetype);
+    if (file.mimetype === 'image/png' || file.mimetype === 'image/jpeg' || file.mimetype === 'image/gif'){
+      cb(null, true);
+    }else{
+      throw new Error('the file is not .png, .jpg or .gif');
+    }
+  }
+}).single('file');
+
+
 app.get('/user', (req: Request, res: Response) => {
   const { protocol, hostname } = req;
   const { name, age } = req.query;
@@ -24,13 +47,12 @@ app.get('/user', (req: Request, res: Response) => {
   res.json(user);
 });
 
-app.get('/upload', (req: Request, res: Response) =>{
-  const { protocol, hostname } = req;
-  const file = req.query;
+app.post('/upload',upload, (req: Request, res: Response) =>{
+  
+  if (!req.file) res.status(400).json({ error: 'No file uploaded' });
+  else res.json({ message: 'File uploaded succsesfully', file: req.file });
 
-
-
-});
+});  
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
