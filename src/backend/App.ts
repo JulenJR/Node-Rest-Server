66f241d16cdf4d02664872848413316f84b94be9
JulenJR <julenjr@hotmail.com>
2023-05-08 12:13:09 +0200

    e 4_1 n2 user data status 401 added

--- src/backend/App.ts
+++ src/backend/App.ts
@@ -1,6 +1,8 @@
 import { Server } from "./Server";
-import express, { Request, Response } from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import multer from 'multer';
+import cors from 'cors';
+
 export class App {
   server?: Server;
 
@@ -24,31 +26,53 @@ const upload = multer({
   dest: '../../upload'
 });
 
+// Cache-Control header middleware
+app.use(function(req: Request, res: Response, next: Function) {
+  res.setHeader('Cache-Control', 'no-cache');
+  next();
+});
+
+// Enable CORS
+app.use(cors());
+
+// Parse JSON in request body
+app.use(express.json());
+
 
 //curl -X POST -F "file=@C:\\Users\\formacio\\Downloads\\image.png" http://localhost:8000/upload
-
-app.post('/upload',upload.single('file'), (req : Request, res : Response) =>{
-  
+app.post('/upload', upload.single('file'), (req : Request, res : Response) => {
   const file = req.file;
   if (!file) {
     res.status(400).json({ error: 'No file uploaded' });
     return;
   }
-
   res.json({ message: 'File uploaded successfully', file: file });
+});
 
+function checkUserData(req: Request, res: Response, next: NextFunction) {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+  next();
+}
+
+//curl -X POST -H "Content-Type: application/json" -d "{\"username\": \"username\", \"password\": \"yourpassword\"}" http://localhost:8000/time
+
+app.post('/time',checkUserData , (req: Request, res: Response) => {
+  const { username } = req.body;
+  const actualDate = new Date().toLocaleString();
+  const result = { time: actualDate, username: username };
+  res.json(result);
 });
 
 app.get('/user', (req: Request, res: Response) => {
   const { protocol, hostname } = req;
   const { name, age } = req.query;
-
   const user = { name: name || 'username', age: age || 6, url: `${protocol}://${hostname}${req.originalUrl}` };
-  console.log('succesfully sent user to http://localhost:8000 in the folder /user')
+  console.log('successfully sent user to http://localhost:8000 in the folder /user')
   res.json(user);
 });
-
-
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
