30bbe4cd4eb71c83e342d2bd12c37a12a0b744e6
JulenJR <julenjr@hotmail.com>
2023-05-17 09:15:00 +0200

    Update Server.ts

--- src/backend/Server.ts
+++ src/backend/Server.ts
@@ -1,27 +1,30 @@
 import { json, urlencoded } from "body-parser";
 import cors from "cors";
-import express from "express";
+import express, {Request, Response, NextFunction} from "express";
 import helmet from "helmet";
+import multer from "multer";
+import axios from "axios";
 
 export class Server {
-	private readonly express: express.Express;
+	private readonly app: express.Express;
 	private readonly port: string;
 
 	constructor(port: string) {
 		this.port = port;
-		this.express = express();
-		this.express.use(helmet());
-		this.express.use(cors());
-		this.express.use(json());
-		this.express.use(urlencoded({ extended: true }));
+		this.app = express();
+		this.app.use(helmet());
+		this.app.use(cors());
+		this.app.use(json());
+		this.app.use(urlencoded({ extended: true }));
 	}
+
 
 	async listen(): Promise<void> {
 		await new Promise<void>((resolve) => {
-			this.express.listen(this.port, () => {
+			this.app.listen(this.port, () => {
 				// eslint-disable-next-line no-console
 				console.log(
-					`✅ Backend App is running at http://localhost:${this.port} in ${this.express.get(
+					`✅ Backend App is running at http://localhost:${this.port} in ${this.app.get(
 						"env"
 					)} mode`
 				);
