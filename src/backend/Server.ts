975683ab812fc949a8729efed9e86bbe3026987c
JulenJR <julenjr@hotmail.com>
2023-05-17 12:27:35 +0200

    const improvements

--- src/backend/Server.ts
+++ src/backend/Server.ts
@@ -1,10 +1,13 @@
 /* eslint-disable*/
-import { json, urlencoded } from "body-parser";
-import cors from "cors";
 import express, { Request, Response, NextFunction } from "express";
 import helmet from "helmet";
+import cors from "cors";
+import { json, urlencoded } from "body-parser";
 import multer, { FileFilterCallback } from "multer";
 import axios from "axios";
+
+const FILE_UPLOAD_DESTINATION = "../../upload";
+const ALLOWED_IMAGE_EXTENSIONS = ["png", "jpg", "jpeg", "gif"];
 
 export class Server {
   private readonly app: express.Express;
@@ -19,52 +22,50 @@ export class Server {
     this.app.use(urlencoded({ extended: true }));
 
     const upload = multer({
-		fileFilter: function (req: Request, file: Express.Multer.File, cb: FileFilterCallback) {
-		  if (!file.originalname.match(/\.(png|jpg|jpeg|gif)$/)) {
-			cb(new Error("Only image files (png, jpg, jpeg, gif) are allowed!"));
-		  } else {
-			cb(null, true);
-		  }
-		},
-		dest: "../../upload"
-	  });
-
-    // Cache-Control header middleware
-    this.app.use(function (req: Request, res: Response, next: NextFunction) {
-      res.setHeader("Cache-Control", "no-cache");
-      next();
+      fileFilter: (req: Request, file: Express.Multer.File, cb: FileFilterCallback) => {
+        if (!file.originalname.match(new RegExp(`\\.(${ALLOWED_IMAGE_EXTENSIONS.join("|")})$`))) {
+          cb(new Error("Only image files (png, jpg, jpeg, gif) are allowed!"));
+        } else {
+          cb(null, true);
+        }
+      },
+      dest: FILE_UPLOAD_DESTINATION,
     });
 
-    this.app.post("/upload", upload.single("file"), (req: Request, res: Response) => {
-		const file = req.file;
-		if (!file) {
-		  res.status(400).json({ error: "No file uploaded" });
-		  return;
-		}
-		res.json({ message: "File uploaded successfully", file: file });
-	  });
-	  
+    const cacheControlMiddleware = (req: Request, res: Response, next: NextFunction) => {
+      res.setHeader("Cache-Control", "no-cache");
+      next();
+    };
 
-    function checkUserData(req: Request, res: Response, next: NextFunction) {
+    const checkUserDataMiddleware = (req: Request, res: Response, next: NextFunction) => {
       const { username, password } = req.body;
       if (!username || !password) {
         return res.status(401).json({ error: "Unauthorized" });
       }
       next();
-    }
+    };
 
-    this.app.post("/time", checkUserData, (req: Request, res: Response) => {
-		const { username } = req.body;
-		const actualDate = new Date().toLocaleString();
-		const result = { time: actualDate, username: username };
-		res.json(result);
-	  });
-	  
+    this.app.use(cacheControlMiddleware);
+
+    this.app.post("/upload", upload.single("file"), (req: Request, res: Response) => {
+      const file = req.file;
+      if (!file) {
+        return res.status(400).json({ error: "No file uploaded" });
+      }
+      res.json({ message: "File uploaded successfully", file });
+    });
+
+    this.app.post("/time", checkUserDataMiddleware, (req: Request, res: Response) => {
+      const { username } = req.body;
+      const actualDate = new Date().toLocaleString();
+      const timeResponse = { time: actualDate, username };
+      res.json(timeResponse);
+    });
 
     this.app.get("/user", (req: Request, res: Response) => {
       const { protocol, hostname } = req;
       const { name, age } = req.body;
-      const user = { name: name, age: age, url: `${protocol}://${hostname}${req.originalUrl}` };
+      const user = { name, age, url: `${protocol}://${hostname}${req.originalUrl}` };
       console.log(`successfully sent user to http://localhost:${port} in the folder /user`);
       res.json(user);
     });
@@ -75,18 +76,14 @@ export class Server {
 
       try {
         const response = await axios.get(url);
-        const data = response.data;
-        const name = data.name;
-        const height = data.height;
-        const weight = data.weight;
-
+        const { name, height, weight } = response.data;
         res.json({ name, height, weight });
       } catch (error) {
         console.error(error);
         res.status(500).send("Internal Server Error");
       }
     });
-}
+  }
 
   async listen(): Promise<void> {
     await new Promise<void>((resolve) => {
