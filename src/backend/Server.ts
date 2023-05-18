3363d9298c7abcf9ecb9ea962b42a69bd8e64b52
JulenJR <julenjr@hotmail.com>
2023-05-18 09:34:47 +0200

    constructor clean

--- src/backend/Server.ts
+++ src/backend/Server.ts
@@ -13,80 +13,87 @@ export class Server {
   constructor(port: string) {
     this.port = port;
     this.app = express();
+    this.setupMiddleware();
+    this.setupRoutes();
+  }
+
+  private setupMiddleware(): void {
     this.app.use(helmet());
     this.app.use(cors());
     this.app.use(json());
     this.app.use(urlencoded({ extended: true }));
 
     const upload = multer({
-		fileFilter: function (req: Request, file: Express.Multer.File, cb: FileFilterCallback) {
-		  if (!file.originalname.match(/\.(png|jpg|jpeg|gif)$/)) {
-			cb(new Error("Only image files (png, jpg, jpeg, gif) are allowed!"));
-		  } else {
-			cb(null, true);
-		  }
-		},
-		dest: "../../upload"
-	  });
+      fileFilter: (req: Request, file: Express.Multer.File, cb: FileFilterCallback) => {
+        if (!file.originalname.match(/\.(png|jpg|jpeg|gif)$/)) {
+          cb(new Error("Only image files (png, jpg, jpeg, gif) are allowed!"));
+        } else {
+          cb(null, true);
+        }
+      },
+      dest: "../../upload",
+    });
 
-    // Cache-Control header middleware
-    this.app.use(function (req: Request, res: Response, next: NextFunction) {
+    this.app.use((req: Request, res: Response, next: NextFunction) => {
       res.setHeader("Cache-Control", "no-cache");
       next();
     });
 
-    this.app.post("/upload", upload.single("file"), (req: Request, res: Response) => {
-		const file = req.file;
-		if (!file) {
-		  res.status(400).json({ error: "No file uploaded" });
-		  return;
-		}
-		res.json({ message: "File uploaded successfully", file: file });
-	  });
-	  
+    this.app.use("/upload", upload.single("file"));
+  }
 
-    function checkUserData(req: Request, res: Response, next: NextFunction) {
+  private setupRoutes(): void {
+    const checkUserData = (req: Request, res: Response, next: NextFunction) => {
       const { username, password } = req.body;
       if (!username || !password) {
         return res.status(401).json({ error: "Unauthorized" });
       }
       next();
+    };
+
+    this.app.post("/upload", this.handleUpload);
+    this.app.post("/time", checkUserData, this.handleTime);
+    this.app.get("/user", this.handleUser);
+    this.app.get("/pokemon/:id", this.handlePokemon);
+  }
+
+  private handleUpload(req: Request, res: Response): void {
+    const file = req.file;
+    if (!file) {
+      res.status(400).json({ error: "No file uploaded" });
+      return;
     }
+    res.json({ message: "File uploaded successfully", file });
+  }
 
-    this.app.post("/time", checkUserData, (req: Request, res: Response) => {
-		const { username } = req.body;
-		const actualDate = new Date().toLocaleString();
-		const result = { time: actualDate, username: username };
-		res.json(result);
-	  });
-	  
+  private handleTime(req: Request, res: Response): void {
+    const { username } = req.body;
+    const actualDate = new Date().toLocaleString();
+    const result = { time: actualDate, username };
+    res.json(result);
+  }
 
-    this.app.get("/user", (req: Request, res: Response) => {
-      const { protocol, hostname } = req;
-      const { name, age } = req.body;
-      const user = { name: name, age: age, url: `${protocol}://${hostname}${req.originalUrl}` };
-      console.log(`successfully sent user to http://localhost:${port} in the folder /user`);
-      res.json(user);
-    });
+  private handleUser(req: Request, res: Response): void {
+    const { protocol, hostname } = req;
+    const { name, age } = req.body;
+    const user = { name, age, url: `${protocol}://${hostname}${req.originalUrl}` };
+    console.log(`successfully sent user to http://localhost:8000 in the folder /user`);
+    res.json(user);
+  }
 
-    this.app.get("/pokemon/:id", async (req: Request, res: Response) => {
-      const id = req.params.id;
-      const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+  private async handlePokemon(req: Request, res: Response): Promise<void> {
+    const id = req.params.id;
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
 
-      try {
-        const response = await axios.get(url);
-        const data = response.data;
-        const name = data.name;
-        const height = data.height;
-        const weight = data.weight;
-
-        res.json({ name, height, weight });
-      } catch (error) {
-        console.error(error);
-        res.status(500).send("Internal Server Error");
-      }
-    });
-}
+    try {
+      const response = await axios.get(url);
+      const { name, height, weight } = response.data;
+      res.json({ name, height, weight });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Internal Server Error");
+    }
+  }
 
   async listen(): Promise<void> {
     await new Promise<void>((resolve) => {
